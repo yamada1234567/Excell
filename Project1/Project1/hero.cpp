04ad6d6a89cb6544d2fb4e4d6bd6528e2c44805d
悠生 岩本 <h1202019014@hiratagakuen.onmicrosoft.com>
2020-10-20 15:56:13 +0900

    syujin

---
+++ Project1/Project1/hero.cpp
@@ -0,0 +1,27 @@
+//使用するヘッダーファイル
+#include "GameL/DrawTexture.h"
+#include "GameHead.h"
+#include "hero.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+
+//イニシャライズ
+void hero::Init()
+{
+
+}
+
+//アクション
+void hero::Action()
+{
+
+}
+
+
+//ドロー
+void hro::Draw()
+{
+
+}
