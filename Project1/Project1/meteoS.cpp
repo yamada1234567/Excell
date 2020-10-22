dd37f4c10fb37c305a3dd13e348052f61544ad0d
陵斗 山田 <h1202015001@hiratagakuen.onmicrosoft.com>
2020-10-22 14:58:15 +0900

    k

---
+++ Project1/Project1/meteoS.cpp
@@ -0,0 +1,22 @@
+#pragma once
+//使用するヘッダー
+#include "GameL/SceneObjManager.h"
+#include "GameHead.h"
+#include "meteoS.h"
+//使用するネームスペース
+using namespace GameL;
+//イニシャライズ
+void CObjmeteoS::Init()
+{
+
+}
+//アクション
+void CObjmeteoS::Action()
+{
+
+}
+//ドロー
+void CObjmeteoS::Draw()
+{
+
+}
