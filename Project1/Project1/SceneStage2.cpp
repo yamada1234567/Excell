1bfefca057f4a39a3759c21369777ee11cde5103
隆 岡本 <h1202019004@hiratagakuen.onmicrosoft.com>
2020-11-09 16:19:17 +0900

    qq

--- Project1/Project1/SceneStage2.cpp
+++ Project1/Project1/SceneStage2.cpp
@@ -57,33 +57,15 @@ void CSceneStage2::Scene()
 
 	if (m_time == 30)
 	{
-		CObjAlien* obj = new CObjAlien(400.0f, 200.0f);
-		Objs::InsertObj(obj, OBJ_Alien, 8);
-		obj->SetVector(0.0f, 1.0f);
-	}
-	if (m_time % 50 == 0)
-	{
-		CObjmeteoRD* obj = new CObjmeteoRD(0.0f, 60.0f);
-		Objs::InsertObj(obj, OBJ_meteoRD, 4);
-		obj->SetVector(1.0f, 1.0f);
-	}
-	if (m_time%50==0)
-	{
-		CObjmeteoLD* obj = new CObjmeteoLD(800.0f, 60.0f);
-		Objs::InsertObj(obj, OBJ_meteoLD, 4);
-		obj->SetVector(1.0f, 1.0f);
-	}
-	if (m_time%60 == 0)
-	{
-		CObjmeteoSin* obj = new CObjmeteoSin(368.0f, 0.0f);
-		Objs::InsertObj(obj, OBJ_meteoSIN, 4);
+		CObjmeteoS* obj = new CObjmeteoS(400.0f, -100.0f);
+		Objs::InsertObj(obj, OBJ_meteoS, 4);
 		obj->SetVector(0.0f, 1.0f);
 	}
 	if (m_time ==200)
 	{
 
 		//ƒNƒŠƒA‚ÉˆÚ“®
-		Scene::SetScene(new CSceneClear(1));
+		//Scene::SetScene(new CSceneClear(1));
 
 
 	}
