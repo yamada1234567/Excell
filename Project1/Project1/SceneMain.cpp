8000f18cbfa4f2f4be517069db3d609e5da0ab75
悠生 岩本 <h1202019014@hiratagakuen.onmicrosoft.com>
2020-11-19 16:43:47 +0900

    f

--- Project1/Project1/SceneMain.cpp
+++ Project1/Project1/SceneMain.cpp
@@ -5,6 +5,7 @@
 //GameLで使用するヘッダー
 #include"GameL/DrawTexture.h"
 #include"GameL\SceneObjManager.h"
+#include"GameL\Audio.h"
 
 //使用するネームスペース
 using namespace GameL;
@@ -26,6 +27,20 @@ CSceneMain::~CSceneMain()
 //ゲームメイン初期化メソッド
 void CSceneMain::InitScene()
 {
+	//音楽読み込み
+	Audio::LoadAudio(0, L"BGMSceneMain.wav",BACK_MUSIC);
+	Audio::LoadAudio(1, L"BGMBoss.wav", BACK_MUSIC);
+
+	Audio::LoadAudio(2,L"SEbullet.wav",EFFECT);
+	Audio::LoadAudio(3, L"SEEnemyexp.wav",EFFECT);
+	Audio::LoadAudio(4, L"SEheroexp.wav", EFFECT);
+
+	//ボリュームを1.5増やす
+	float v = Audio::VolumeMaster(1.5);
+
+	//音楽スタート
+	Audio::Start(0);
+
 	//外部グラフィックファイルを読み込む0番に登録()
 	Draw::LoadImage(L"hero.png", 0, TEX_SIZE_512);
 	Draw::LoadImage(L"hero bullet.png", 1, TEX_SIZE_512);
@@ -130,8 +145,12 @@ void CSceneMain::Scene()
 	}
 	if (m_time == 1200)
 	{
+		//音楽チェンジ
+		Audio::Stop(0);//0番曲をストップ
+		Audio::Start(1);//1番曲をスタート
+
 		x = 100;
-		CObjBoss* obj = new CObjBoss(x, -650.0f);
+		CObjBoss* obj = new CObjBoss(x, 10.0f);
 		Objs::InsertObj(obj, OBJ_BOSS, 6);
 	}
 }
